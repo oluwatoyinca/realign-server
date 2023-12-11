0616c5c14cc18e984631a7527d8b769bbb5b1305
Oluwatoyin Amushan <alexandradonalds@gmail.com>
2023-12-11 15:00:29 -0700

    Flesh out ads-related-visits router post and get methods

--- routers/ads-related-visits.js
+++ routers/ads-related-visits.js
@@ -2,8 +2,35 @@ const express = require('express')
 const router = new express.Router()
 const { pool } = require('../db/connect')
 
-router.post('/ads-visit', async (req, res) => {})
+const passBasicChecks = (paramsArray) => {
+    const hasInvalidParams = paramsArray.some(param => param[0] == null || param[0].length == 0 || param[0].length > param[1])
+    return !hasInvalidParams
+}
 
-router.get('/ads-visit', async (req, res) => {})
+router.post('/ads-visit', async (req, res) => {
+    const { utm_campaign, page} = req.body
+    if (passBasicChecks([[utm_campaign, 300], [page, 100]])) {
+        try {
+            const insertQuery = 'INSERT INTO ads_related_visits (utm_campaign, page_slug) VALUES($1, $2)'
+            const insertValues = [utm_campaign, page]
+            
+            const insertResponse = await pool.query(insertQuery, insertValues)
+            res.status(201).send({ data: insertResponse.rows })
+        } catch(e) {
+            res.status(500).send(e)
+        }
+    }
+    else res.status(400).send(new Error('Invalid arguments!'))
+})
+
+router.get('/ads-visit', async (req, res) => {
+    try {
+        const checkQuery = 'select * FROM ads_related_visits'
+        const checkResponse = await pool.query(checkQuery)
+        res.status(200).send({ data: checkResponse.rows })
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
 
 module.exports = {router}
