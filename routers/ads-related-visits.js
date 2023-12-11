ef910ded198891fc7e50e92eadc6d1329f6e7200
Oluwatoyin Amushan <alexandradonalds@gmail.com>
2023-12-11 14:20:20 -0700

    Create routers and api methods

---
+++ routers/ads-related-visits.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = new express.Router()
+
+router.post('/ads-visit', async (req, res) => {})
+
+router.get('/ads-visit', async (req, res) => {})
+
+module.exports = {router}
---
+++ routers/campaign-conversions.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = new express.Router()
+
+router.post('/conversions', async (req, res) => {})
+
+router.get('/conversions', async (req, res) => {})
+
+router.patch('/conversions/:id', async (req, res) => {})
+
+module.exports = {router}
